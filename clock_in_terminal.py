ad2becdc3f98e42eaa8c8eb1e71a6ca2d14e39d7
moRhen <morhenmail@gmail.com>
2015-09-30 12:51:32 +0200

    auto center in realtime depend on terminal size

--- clock_in_terminal.py
+++ clock_in_terminal.py
@@ -1,4 +1,7 @@
+#!/usr/bin/env python3
+
 import time
+import os
 
 a1 = [[' ', ' ', '#', ' '], [' ', '#', '#', ' '], ['#', ' ', '#', ' '], [' ', ' ', '#', ' '], [' ', ' ', '#', ' '], [' ', '#', '#', '#']]
 a2 = [[' ', '#', '#', ' '], ['#', ' ', ' ', '#'], [' ', ' ', '#', ' '], [' ', '#', ' ', ' '], ['#', ' ', ' ', ' '], ['#', '#', '#', '#']]
@@ -14,6 +17,9 @@ aa = [[' ', ' ', ' ', ' '], [' ', ' ', ' ', ' '], [' ', ' ', ' ', ' '], [' ', ' 
 
 numbers = {'1':a1, '2':a2, '3':a3, '4':a4, '5':a5, '6':a6, '7':a7, '8':a8, '9':a9, '0':a0}
 
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 def time_in_num():
     what_time = time.strftime('%H%M%S')
     hour0 = numbers[what_time[0]]
@@ -22,7 +28,7 @@ def time_in_num():
     minute1 = numbers[what_time[3]]
     second0 = numbers[what_time[4]]
     second1 = numbers[what_time[5]]
-
+    
     printing_time_table = []
     n = 0
     while n < 6:
@@ -30,12 +36,17 @@ def time_in_num():
         n+=1
     return printing_time_table
 
-
 def print_clock():
     while True:
-        print('\n' * 100)
+        clear()
         time_table = time_in_num()
+        rowsnumb, columns = os.popen('stty size', 'r', 1).read().split()
+        centercol = (' ' * int(((int(columns)) - 63) // 2))
+        rowcenter = ('\n' * int(((int(rowsnumb)) - 7) // 2))
+        print(rowcenter)
         for rows in time_table:
+            rows.insert(0, centercol)
             print(' '.join(rows))
         time.sleep(1)
+
 print_clock()
